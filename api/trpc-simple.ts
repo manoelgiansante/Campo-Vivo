5282d980e70d0873d685a06b4732314e92965a7a
manoelgiansante <manoelgiansante@gmail.com>
2025-12-11 22:55:13 -0300

    Add trpc-simple test

---
+++ api/trpc-simple.ts
@@ -0,0 +1,9 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).json({
+    status: "trpc-simple working",
+    url: req.url,
+    method: req.method
+  });
+}
